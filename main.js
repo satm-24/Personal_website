547f9080bbf6812bfdd2d1fbf8e6b40370582d3c
Satwik Misra <misra.sa@northeastern.edu>
2021-11-01 03:29:30 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
+  const toggle = document.getElementById(toggleId),
+  nav = document.getElementById(navId)
+
+  if(toggle && nav){
+      toggle.addEventListener('click', ()=>{
+          nav.classList.toggle('show')
+      })
+  }
+}
+showMenu('nav-toggle','nav-menu')
+
+/*===== ACTIVE AND REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav__link');   
+
+function linkAction(){
+/*Active link*/
+navLink.forEach(n => n.classList.remove('active'));
+this.classList.add('active');
+
+/*Remove menu mobile*/
+const navMenu = document.getElementById('nav-menu')
+navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
+
+function copyText() {
+
+  const email = "misra.sa@northeastern.edu"
+
+  navigator.clipboard.writeText(email).then(function() {
+    console.log('Async: Copying to clipboard was successful!');
+  }, function(err) {
+    console.error('Async: Could not copy text: ', err);
+  });
+
+}
+
+const toggleButton = document.getElementsByClassName('nav__toggle')[0]
+const navLinks = document.getElementsByClassName('nav__menu')[0]
+
+console.log(toggleButton);
+console.log(navLinks);
+
+
+toggleButton.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+})
+
+
+
+
